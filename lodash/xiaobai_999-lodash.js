c0ab26b89b42183cfd4477e2960eda60017e683b
CC <738282509@qq.com>
2022-09-14 18:29:56 +0800

    test

--- lodash/xiaobai_999-lodash.js
+++ lodash/xiaobai_999-lodash.js
@@ -1,3 +1,26 @@
-var xiaobai_999={
+var xiaobai_999 = {
+
+  chunk(array, size) {
+    let res = []
+    let index = 0;
+    let pos = [];
+    for (let i = 0; i < array.length; i++) {
+      if (index === size) {
+        res.push(pos);
+        pos = []
+        index = 0
+      }
+      index++;
+      pos.push(array[i]);
+    }
+    if (index !== 0) {
+      res.push(pos);
+      pos = []
+      index = 0
+    }
+    return res;
+  }
+
+
 
 }
